bf95b448ca7539844a46dee60886559e17b3535b
João Luís <j13domingos@gmail.com>
2018-06-10 21:25:48 -0300

    Feito o algoritimo que criptografa um texto e decriptografa o mesmo.

---
+++ venv/src/__init__.py
@@ -0,0 +1,34 @@
+alfabeto = 'abcdefghijklmnopqrstuvwxyz'
+
+def cifrar(texto, chave):
+    texto_cifrado = ''
+
+    i = 0
+    for letra in texto:
+        soma = alfabeto.find(letra) + alfabeto.find(chave[i % len(chave)])
+        modulo = int(soma) % len(alfabeto)
+        texto_cifrado = texto_cifrado + str(alfabeto[modulo])
+        i += 1
+    return texto_cifrado
+
+def decifrar(texto, chave):
+    texto_cifrado = ''
+
+    i = 0
+    for letra in texto:
+        soma = alfabeto.find(letra) - alfabeto.find(chave[i % len(chave)])
+        modulo = int(soma) % len(alfabeto)
+        texto_cifrado = texto_cifrado + str(alfabeto[modulo])
+        i += 1
+    return texto_cifrado
+
+def main():
+    c = str(input('Texto a criptografar: ')).lower()
+    chave = str(input('Chave: ')).lower()
+    print (cifrar(c, chave))
+    c = str(input('Texto a descriptografar: ')).lower()
+    chave = str(input('Chave: ')).lower()
+    print (decifrar(c, chave))
+
+if __name__ == '__main__':
+    main()
